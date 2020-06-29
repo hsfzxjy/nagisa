43374828e1a9aead0158726dfa70863c5df4cb33
hsfzxjy <hsfzxjy@gmail.com>
2020-06-30 00:23:18 +0800

    Black ast.py

--- nagisa/utils/primitive/ast.py
+++ nagisa/utils/primitive/ast.py
@@ -2,6 +2,7 @@ import ast
 from nagisa.utils.primitive import typing
 from nagisa.utils.primitive.malformed import Malformed
 
+
 def node_to_object(node):
     if isinstance(node, (ast.List, ast.Tuple)):
         return [node_to_object(n) for n in node.elts]
