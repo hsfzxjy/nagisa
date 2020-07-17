e833887027fd348b1d448c6bff9c3520d6eb0c40
hsfzxjy <hsfzxjy@gmail.com>
2020-07-17 18:16:54 +0800

    Add missing test.

--- tests/io_/test_file.py
+++ tests/io_/test_file.py
@@ -35,14 +35,14 @@ class TestDownloadUrlToFile(TorchTestCase):
         self.assertTensorEqual(data["tensor"], torch.tensor(range(100), dtype=float))
         self.assertEqual(data["string"], "hello world")
 
-    # @skip_if_local
-    # def test_big_file(self):
-    #     # A ~40MB zip file which requires confirm code
-    #     download_url_to_file(
-    #         "https://drive.google.com/file/d/1X0NTkVNlXBvC0uRj1vWl4Ow7MA4bNQtk/view",
-    #         self.dest,
-    #         hash_prefix="71c70feee092db533f959362f866896afa59a57fb04c290f16667a37882589d3",
-    #     )
+    @skip_if_local
+    def test_big_file(self):
+        # A ~40MB zip file which requires confirm code
+        download_url_to_file(
+            "https://drive.google.com/file/d/1X0NTkVNlXBvC0uRj1vWl4Ow7MA4bNQtk/view",
+            self.dest,
+            hash_prefix="71c70feee092db533f959362f866896afa59a57fb04c290f16667a37882589d3",
+        )
 
 
 class TestLoadStateDict(TorchTestCase):
